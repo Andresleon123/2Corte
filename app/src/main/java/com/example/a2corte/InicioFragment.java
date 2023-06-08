a5b9616f0e992133b1b7f687ab2871a144316ed8
Andresleon123 <leon38978@gmail.com>
2023-06-08 11:22:29 -0500

    agreggar un espacio

--- app/src/main/java/com/example/a2corte/InicioFragment.java
+++ app/src/main/java/com/example/a2corte/InicioFragment.java
@@ -3,6 +3,10 @@ package com.example.a2corte;
 import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -10,11 +14,6 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.example.a2corte.data.Daolibreria;
 import com.example.a2corte.data.Persona;
